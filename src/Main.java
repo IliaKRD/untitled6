8529756ef7b5e910e6749e5e2702b61b808cb722
IliaKRD <korenovskpark@mail.ru>
2024-02-06 22:20:57 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.util.HashSet;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите набор чисел в виде одной строки: ");
+        String input = scanner.nextLine();
+
+        String[] numbersStr = input.split(",");
+        HashSet<Integer> numbers = new HashSet<Integer>();
+        for (int i = 0; i < numbersStr.length; i++) {
+            numbers.add(Integer.parseInt(numbersStr[i]));
+        }
+
+        // Выведите массив чисел на консоль для проверки
+        System.out.println("Избавились от повторяющихся значений:");
+        for (int number : numbers) {
+            System.out.print(number + " ");
+        }
+    }
+}
